3ddd198cce2f41bc28e37c1b907e22e2f2e8cee1
rankun203 <rankun203@gmail.com>
2015-01-24 22:32:06 +0800

    update bodyType to html

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ var Email = require('email').Email;
 
 //var url = 'http://wb.zk789.cn/';
 var url = 'http://www.baidu.com';
+var actions = {
+    statusCode: 'statusCode',
+    grep: 'grep',
+    copy: 'copy'
+};
+var urls = [
+    {
+        url: 'http://wb.zk789.cn/',
+        action: actions.statusCode,
+        expect: 200
+    },{
+        url: 'http://www.51test.net/zikao/sichuan/baoming/',
+        action: actions.grep,
+        expect: /2015年4月四川.*报名时间/
+    },{
+        url: 'http://www.51test.net/zikao/sichuan/siyue/',
+        action: actions.grep,
+        expect: /2015年4月四川.*报名时间/
+    },{
+        url: 'http://www.233.com/zikao/baoming/',
+        action: actions.copy,
+        search: //
+    }
+];
+//'http://wb.zk789.cn/',
+//    'http://www.51test.net/zikao/sichuan/baoming/',
+//    'http://www.51test.net/zikao/sichuan/siyue/',
+//    'http://www.233.com/zikao/baoming/'
 var from = 'rankun203@gmail.com';
 var to = 'rankun203@icloud.com';
 
@@ -13,7 +41,8 @@ function sendMail(subject, body) {
         from: from,
         to: to,
         subject: subject,
-        body: body
+        body: body,
+        bodyType: 'html'
     });
     myMsg.send(function(err) {
         console.error("ERROR: " + err);
