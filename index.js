a3ac556030ae96a3ca949c4e47c3ebb125483740
rankun203 <rankun203@gmail.com>
2015-01-24 22:57:40 +0800

    update bodyType to html

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 var request = require('request');
 var Email = require('email').Email;
 
-//var url = 'http://wb.zk789.cn/';
-var url = 'http://www.baidu.com';
 var actions = {
     statusCode: 'statusCode',
     grep: 'grep',
@@ -23,18 +21,11 @@ var urls = [
         url: 'http://www.51test.net/zikao/sichuan/siyue/',
         action: actions.grep,
         expect: /2015年4月四川.*报名时间/
-    },{
-        url: 'http://www.233.com/zikao/baoming/',
-        action: actions.copy,
-        search: /.*/
     }
 ];
-//'http://wb.zk789.cn/',
-//    'http://www.51test.net/zikao/sichuan/baoming/',
-//    'http://www.51test.net/zikao/sichuan/siyue/',
-//    'http://www.233.com/zikao/baoming/'
 var from = 'rankun203@gmail.com';
 var to = 'rankun203@icloud.com';
+
 
 function sendMail(subject, body) {
     var myMsg = new Email({
@@ -49,12 +40,28 @@ function sendMail(subject, body) {
     });
 }
 
-request(url, function (error, response, body) {
-    if(error) throw error;
-    if(response.statusCode == 200) {
-        console.log('Success');
-        sendMail("Website accessible!!", body);
-    } else if(response.statusCode == 404) {
-        console.log('Not found');
-    }
-});
+for(var _url in urls) {
+    request(_url.url, function (error, response, body) {
+        if(error) throw error;
+
+        var notFound = false;
+        switch (_url.action) {
+            case actions.statusCode:
+                if(response.statusCode == 200) {
+                    console.log('Success');
+                    sendMail("Website accessible!!", body);
+                } else if(response.statusCode == 404) {
+                    console.log('Not found');
+                    notFound = true;
+                } else notFound = true;
+                break;
+            case actions.grep:
+                var _tmpHtml = body.match(_url.expect);
+                if(_tmpHtml == null) sendMail(_url.url + "找到了", _tmpHtml + ":\n" + _url.url);
+                else notFound = true;
+                break;
+        }
+        if(notFound) sendMail("什么都没有找到", JSON.stringify(urls));
+    });
+}
+
